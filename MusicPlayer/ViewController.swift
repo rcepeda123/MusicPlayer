20ba96d3a59ca93c2ac5871b7dc00ac26a92553a
Raymond <raymondcepeda456@gmail.com>
2019-07-17 23:13:22 -0400

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Raymond Cepeda on 7/17/19.
+//  Copyright © 2019 Raymond Cepeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
